a95c4733ebce12062fd76251ce09436597ea773e
kyawakyawa <kyawashell@gmail.com>
2020-09-19 17:08:24 +0900

    abc016

---
+++ atcoder/abc016/b.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+  int A, B, C;
+  cin >> A >> B >> C;
+
+  if (A + B == C && A - B != C) {
+    cout << "+" << endl;
+  } else if (A + B != C && A - B == C) {
+    cout << "-" << endl;
+  } else if (A + B == C && A - B == C) {
+    cout << "?" << endl;
+  } else {
+    cout << "!" << endl;
+  }
+  return 0;
+}
