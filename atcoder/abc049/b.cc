f014597c28432f89d79b6f5a8d5247cf35d93edc
kyawakyawa <kyawashell@gmail.com>
2020-10-24 23:04:50 +0900

    abc049b

---
+++ atcoder/abc049/b.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+  int H, W;
+  cin >> H >> W;
+  for (int i = 0; i < H; ++i) {
+    string C;
+    cin >> C;
+    cout << C << endl;
+    cout << C << endl;
+  }
+  return 0;
+}
