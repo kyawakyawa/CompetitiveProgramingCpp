ec7d58bdbcfed822f23c2a2ac5248a8a3a9ab08b
kyawakyawa <kyawashell@gmail.com>
2020-12-20 21:32:29 +0900

    arc016a

---
+++ atcoder/arc016/a.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+  int N, M;
+  cin >> N >> M;
+  if (M == 1) {
+    cout << 2 << endl;
+  } else {
+    cout << 1 << endl;
+  }
+  return 0;
+}
